09f308c6a94beaa27a12fef9040ece30e81e37ef
Gourav Dhiman <150810342+gouravdhiman08@users.noreply.github.com>
2024-11-13 18:53:05 +0530

    fghghh

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Agriculture with Arjun',
+      title: 'Agriculture wth Arjun',
       initialRoute: AppRoutes.splash,
       getPages: AppRoutes.getPages,
     );
