c2801bd8b5d42cbc92cb131239e1a0e5998064cf
Gourav Dhiman <150810342+gouravdhiman08@users.noreply.github.com>
2024-11-19 10:18:41 +0530

    firebase branch setup

---
+++ lib/core/Firebase/firebase.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Firebase extends StatelessWidget {
+  const Firebase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
